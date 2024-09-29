f7146905c6534e3f86b82fe3781d373ef850601c
liya <omarovaam@bmstu.ru>
2024-09-29 22:30:09 +0300

    changes in main page

--- src/js/views/Feed.js
+++ src/js/views/Feed.js
@@ -9,38 +9,43 @@ export default class extends AbstractView {
     async getHtml() {
         return `
             <div class="container">
-            <div class="form-section">
-            <div class="logo">
-                <img src="../assets/img/Logo.svg" alt="Logo">
-            </div>
-            <h1 class="description"><Логин></h1>
+                <div class="image-section-wrapper">
+                    <div class="image-section">
+                        <!-- Поле для фотографии пользователя (пока пустое) -->
+                        <div class="user-photo-placeholder">
+                            <img src="../assets/img/placeholder.png" alt="User Photo Placeholder">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-section">
+                    <h1 class="description"><Логин></h1>
+                    <label for="information" class="text-base">Информация о пользователе</label>
+                    <p>Здесь будет информация о пользователе.</p>
 
-            <label for="information" class=".text-base">Информация о пользователе</label>
-            
-
-            <label for="additional inf" class=".text-base">...</label>
-            
-
-            
-
-            <div class="card-actions">
-                    <button class="btn skip">
-                        <span>Пропустить</span>
+                    <button class="btn custom-btn">
+                        <img src="../assets/img/X.svg" alt="X">
                     </button>
-                    <button class="btn like">
-                        <span>Нравится</span>
+                    <button class="btn custom-btn">
+                        <img src="../assets/img/Heart.svg" alt="Heart">
                     </button>
-                    <button class="btn photo">
-                        <span>Листать фото</span>
-                    </button>
-                    <button class="btn gift">
-                        <span>Сделать подарок</span>
-                    </button>
-            </div>
-
-            
-            </div>
+                    <div class="card-actions">
+                        <button class="btn skip">
+                            <span>Пропустить</span>
+                        </button>
+                        <button class="btn like">
+                            <span>Нравится</span>
+                        </button>
+                        <button class="btn photo">
+                            <span>Листать фото</span>
+                        </button>
+                        <button class="btn gift">
+                            <span>Сделать подарок</span>
+                        </button>
+                        
+                    </div>
+                </div>
             </div>
         `;
     }
+    
 }
