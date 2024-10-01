b377881264dacbcfe6c26467cdde8df4f53bb175
Elizabeth <liza777sem@icloud.com>
2024-10-01 13:33:25 +0300

    corrections in cards styles

--- src/js/views/CardView.js
+++ src/js/views/CardView.js
@@ -9,39 +9,39 @@ export default class extends AbstractView {
     async getHtml() {
         return `
             <div class="tinder">
-  <div class="tinder--cards">
-    <div class="tinder--card">
-      <img src="../assets/img/logo.svg">
-      <h3>Demo card 1</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="../assets/img/logo.svg">
-      <h3>Demo card 2</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="../assets/img/logo.svg">
-      <h3>Demo card 3</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="../assets/img/logo.svg">
-      <h3>Demo card 4</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-    <div class="tinder--card">
-      <img src="../assets/img/logo.svg">
-      <h3>Demo card 5</h3>
-      <p>This is a demo for Tinder like swipe cards</p>
-    </div>
-  </div>
+              <div class="tinder--cards">
+                <div class="tinder--card">
+                  <img src="../assets/img/logo.svg">
+                  <h3>Demo card 1</h3>
+                  <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                  <img src="../assets/img/logo.svg">
+                  <h3>Demo card 2</h3>
+                  <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                  <img src="../assets/img/logo.svg">
+                  <h3>Demo card 3</h3>
+                  <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                  <img src="../assets/img/logo.svg">
+                  <h3>Demo card 4</h3>
+                  <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+                <div class="tinder--card">
+                  <img src="../assets/img/logo.svg">
+                  <h3>Demo card 5</h3>
+                  <p>This is a demo for Tinder like swipe cards</p>
+                </div>
+              </div>
 
-  <div class="tinder--buttons">
-    <button id="nope"><i class="fa fa-remove"></i></button>
-    <button id="love"><i class="fa fa-heart"></i></button>
-  </div>
-</div>
+              <div class="tinder--buttons">
+                <button id="nope"><i class="fa fa-remove"></i></button>
+                <button id="love"><i class="fa fa-heart"></i></button>
+              </div>
+            </div>
         `;
     }
 }
