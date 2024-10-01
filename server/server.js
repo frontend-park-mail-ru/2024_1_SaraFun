2e8ea915816fe7793224833b4eb566f67a1bccdc
KranjQ <easypandaoo@gmail.com>
2024-10-01 09:17:13 +0300

    -

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../src", "index.html"));
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
