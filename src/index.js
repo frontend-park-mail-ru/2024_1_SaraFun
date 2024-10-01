2b34c0faece7293fbc9f2af974fbf1786fee65e6
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-10-01 17:54:34 +0300

    Update index.js
    сообщение в консоль, что авторизация и регистрация прошли успешно

--- src/index.js
+++ src/index.js
@@ -291,7 +291,7 @@ const router = async () => {
             if (!response.ok) {
                 throw new Error('Ошибка регистрации');
             }
-    
+            console.log('Успешно зарегестрировался')
             // window.location.href = '/feed';
     
         } catch (error) {
@@ -350,7 +350,7 @@ const router = async () => {
                 throw new Error('Ошибка входа');
             }
     
-            
+            console.log('Успешно авторизовался')
             // window.location.href = '/feed';
     
         } catch (error) {
