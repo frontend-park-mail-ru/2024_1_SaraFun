c676f7fb7f11ffafc7dc48dc8ae9b1d797f9d5dc
Elizabeth <liza777sem@icloud.com>
2024-10-01 20:13:56 +0300

    corrections

--- src/index.js
+++ src/index.js
@@ -441,3 +441,14 @@ const router = async () => {
 };
 
 window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
