4497344d7cb27222353dbab108134fbd8f745101
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-10-12 19:05:51 +0300

    created ajax in api.js

--- src/app/api.js
+++ src/app/api.js
@@ -1,100 +1,66 @@
-async function logout() {
-    try {
-        const response = await fetch('http://5.188.140.7:8080/logout', {
-            method: 'GET',
-            credentials: 'include' 
-        });
-        if (!response.ok) {
-            return false;
-        }
-        return true;
-    }
-    catch (error){
-        //console.log('error logout')
-        return false;
-    }
-}
-
-async function loginUser(login, password) {
-    try {
-        //console.log(login, password);
-        const response = await fetch('http://5.188.140.7:8080/signin', { 
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials : 'include',
-            body: JSON.stringify({'username': login, 'password': password}),
-        });
-
-        if (!response.ok) {
-            return false
-        }
-
-        //console.log('Успешно авторизовался');
-        await renderNavBar();
-        navigateTo('/feed');
-        return true
-    } catch (error) {
-        // console.error('Ошибка:', error);
-        return false
-    }
-}
-
-async function checkAuth() {
-    try {
-        const response = await fetch('http://5.188.140.7:8080/checkauth', {
-            method: 'GET',
-            credentials: 'include' 
-            });
-        if (!response.ok) {
-            return false;
-        }
-        return true;
-    }
-    catch (error){
-        //console.log('error auth')
-        return false;
-    }
-}
-
-async function fetchUsers() {
-    try {
-      const response = await fetch('http://5.188.140.7:8080/getusers', {
+function logout() {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/logout',
         method: 'GET',
-        credentials: 'include',
-        mode: 'cors'
-      });
-      if (!response.ok) {
-        throw new Error('Ошибка при получении списка пользователей');
-      }
-      return await response.json();
-    } catch (error) {
-      console.error('Ошибка при получении списка пользователей:', error);
-      return [];
-    }
+        xhrFields: {
+            withCredentials: true
+        }
+    }).then(() => true).catch(() => false);
 }
 
-async function registerUser(login, password, gender, age) {
-    try {
-      //console.log(login, password, gender, age);
-      const response = await fetch('http://5.188.140.7:8080/signup', { 
+function loginUser(login, password) {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/signin',
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
+        contentType: 'application/json',
+        xhrFields: {
+            withCredentials: true
         },
-        credentials : 'include',
-        body: JSON.stringify({'username': login, 'password': password, 'gender': gender, 'age': parseInt(age)}),
-      });
-    
-      if (!response.ok) {
-        // throw new Error('Ошибка регистрации');
-      }
-      //console.log('Успешно зарегистрировался');
-      await renderNavBar();
-      navigateTo('/feed');
-    
-    } catch (error) {
-      console.error('Ошибка:', error);
-    }
+        data: JSON.stringify({ username: login, password: password })
+    }).then(() => {
+        renderNavBar();
+        navigateTo('/feed'); //тут ли это должно быть?
+        return true;
+    }).catch(() => false);
+}
+
+function checkAuth() {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/checkauth',
+        method: 'GET',
+        xhrFields: {
+            withCredentials: true
+        }
+    }).then(() => true).catch(() => false);
+}
+
+function fetchUsers() {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/getusers',
+        method: 'GET',
+        xhrFields: {
+            withCredentials: true
+        },
+        dataType: 'json'
+    }).then(data => data).catch(error => {
+        console.error('Ошибка при получении списка пользователей:', error);
+        return [];
+    });
+}
+
+function registerUser(login, password, gender, age) {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/signup',
+        method: 'POST',
+        contentType: 'application/json',
+        xhrFields: {
+            withCredentials: true
+        },
+        data: JSON.stringify({ username: login, password: password, gender: gender, age: parseInt(age) })
+    }).then(() => {
+        renderNavBar();
+        navigateTo('/feed'); //тут ли это должно быть?
+    }).catch(error => {
+        console.error('Ошибка:', error);
+    });
 }
