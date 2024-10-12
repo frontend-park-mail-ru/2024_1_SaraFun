f05e1fc603691ebb20fab9c379c5de7414ce9211
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-10-12 19:11:07 +0300

    Add files via upload

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,69 @@
+import templateFeed from '../Compile/feed.js';
+import templateLogin from '../Compile/login.js';
+import templateSignup from '../Compile/signup.js';
+import templateCard from '../Compile/SwipeCard.js';
+
+function renderFeed (parent) {
+	function createCard(user) {
+		return `
+			<div class="tinder--card">
+				<div class="image-section">
+					<img src="../assets/img/image.svg" alt="Image" draggable="false">
+				</div>
+				<div class="form-section-tinder">
+					<h1>${user.username}</h1>
+					<text-base>Пол: ${user.gender}</text-base>
+					<text-base>Возраст: ${user.age}</text-base>
+				</div>
+			</div>
+		`;
+	}
+	function initCards() { 
+		var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
+	
+		newCards.forEach(function (card, index) {
+		  card.style.zIndex = allCards.length - index;
+		  card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+		  card.style.opacity = (10 - index) / 10;
+		});
+			  
+		tinderContainer.classList.add('loaded');
+	}
+	parent.innerHTML = '';
+	const users = [ { "id": 1, "username": "Andrey", "age": 20, "gender": "male"}, { "id": 2, "username": "Anton", "age": 20, "gender": "male"}, { "id": 2, "username": "Anton", "age": 20, "gender": "male"},{ "id": 2, "username": "Anton", "age": 20, "gender": "male"},{ "id": 2, "username": "Anton", "age": 20, "gender": "male"},{ "id": 2, "username": "Anton", "age": 20, "gender": "male"},{ "id": 2, "username": "Anton", "age": 20, "gender": "male"},{ "id": 2, "username": "Anton", "age": 20, "gender": "male"},{ "id": 2, "username": "Anton", "age": 20, "gender": "male"},{ "id": 2, "username": "Anton", "age": 20, "gender": "male"} ];
+	parent.innerHTML = templateFeed();
+	const tinderContainer = document.querySelector('.tinder--cards');
+	//tinderContainer.innerHTML = templateCard(users[0]);
+	tinderContainer.innerHTML = users.map(user => createCard(user)).join('');
+	var allCards = document.querySelectorAll('.tinder--card');
+	initCards();
+};
+
+function renderLogin (parent) {
+	parent.innerHTML = '';
+	parent.innerHTML = templateLogin();
+};
+
+function renderSignup (parent) {
+	parent.innerHTML = '';
+	parent.innerHTML = templateSignup();
+};
+
 export const createRouter = (root) => ({
     feed: {
       path: '/feed',
       handle: 'Главная',
-      componentName: new MainPage(root),
+      componentName: renderFeed,
     },
     login: {
       path: '/login',
-      handle: 'Авторизоваться',
-      componentName: new LoginPage(root),
+      handle: 'Авторизация',
+      componentName: renderLogin,
     },
     signup: {
       path: '/signup',
       handle: 'Регистрация',
-      componentName: new RegistrationPage(root)
+      componentName: renderSignup
     },
 });
 
@@ -20,10 +71,8 @@ export default class App {
 	#state = {};
 	handlers = {};
 	#structure = {};
-	config;
 	root;
-	constructor(config, root) {
-		this.config = config;
+	constructor(root) {
 		this.root = root;
 		this.#state.user = null;
         this.router = createRouter(root);
@@ -34,11 +83,11 @@ export default class App {
         if (route) {
             history.pushState({}, '', route.path);
             this.clear(true);
-            route.componentName.render();
+            route.componentName(root);
             this.#structure[route.handle] = route.componentName;
         } else {
             history.pushState({}, '', this.router.feed.path);
-            this.#renderFeed();
+			this.goToPage(this.router.login.path);
         }
 	}
 
@@ -54,145 +103,5 @@ export default class App {
 				delete this.#structure[key];
 			}
 		});
-	}
-
-	#renderMenu() {
-		const menu = new Menu(this.config.homeConfig.menu, this.root);
-		if (!this.#structure.menu) {
-			this.#structure.menu = menu;
-			menu.render();
-		}
-
-		menu.addHandler(
-			menu.htmlElement.querySelector('a[data-section="feed"]'),
-			'click',
-			(event) => {
-				event.preventDefault();
-				this.goToPage(this.router.feed.path);
-			},
-		);
-		menu.addHandler(
-			menu.htmlElement.querySelector('a[data-section="login"]'),
-			'click',
-			(event) => {
-				event.preventDefault();
-				this.goToPage(this.router.login.path, true);
-			},
-		);
-		menu.addHandler(
-			menu.htmlElement.querySelector('a[data-section="signup"]'),
-			'click',
-			(event) => {
-				event.preventDefault();
-				this.goToPage(this.router.signup.path, true);
-			},
-		);
-	}
-
-	#renderFeed() {
-		const config = this.config.homeConfig;
-
-		this.#renderMenu();
-
-		const main = new Container({ key: 'main', ...config.main }, this.root);
-		main.render();
-		this.#structure.main = main;
-
-		const header = new Header(
-			{ key: 'header', ...config.main.header },
-			main.htmlElement,
-		);
-		header.render();
-		this.#structure.main.header = header;
-
-		const content = new Container(
-			{ key: 'content', ...config.main.content },
-			main.htmlElement,
-		);
-		content.render();
-		this.#structure.main.content = content;
-
-		const aside = new Container(
-			{
-				key: 'aside',
-				...config.main.aside,
-			},
-			main.htmlElement,
-		);
-		aside.render();
-		this.#structure.main.aside = aside;
-
-		const logoutButton = header.htmlElement.querySelector(
-			'img.header-profile-logout',
-		);
-		const logoutButtonHandler = () => {
-			Ajax.post('/auth/logout', {}, (data, error) => {
-				if (error) {
-					console.log('logoutError:', error);
-				}
-				this.goToPage(router.login, true);
-			});
-		};
-		header.addHandler(logoutButton, 'click', logoutButtonHandler);
-	}
-
-	#renderSignup() {
-		const config = this.config.signupConfig;
-		const signUp = new SignupForm(config, this.root);
-		signUp.render();
-		this.#structure.signUp = signUp;
-
-		const signupForm = signUp.htmlElement.querySelector('form');
-		const submitHandler = (event) => {
-			event.preventDefault();
-			const data = validateForm(config.inputs, signupForm);
-			if (data) {
-				Ajax.sendForm('/auth/signup', data, (response, error) => {
-					if (response.ok) {
-						this.goToPage(router.feed, true);
-					} else {
-						console.log('status:', response.statusText);
-					}
-				});
-			}
-		};
-		signUp.addHandler(signupForm, 'submit', submitHandler);
-
-		const toLoginLink = signUp.items.toLoginLink;
-		const clickHandler = (event) => {
-			event.preventDefault();
-			this.goToPage(router.login, true);
-		};
-		toLoginLink.addHandler('click', clickHandler);
-	}
-
-	#renderLogin() {
-		const config = this.config.loginConfig;
-		const login = new LoginForm(config, this.root);
-		login.render();
-		this.#structure.login = login;
-
-		const loginForm = login.htmlElement.querySelector('form');
-		const submitHandler = (event) => {
-			event.preventDefault();
-			const data = validateForm(config.inputs, loginForm);
-			if (data) {
-				Ajax.sendForm('/auth/login', data, (response, error) => {
-					if (response.ok) {
-						this.goToPage(router.feed, true);
-					} else {
-						console.log('status:', response.statusText);
-					}
-				});
-			}
-		};
-		login.addHandler(loginForm, 'submit', submitHandler);
-
-		const toSignupLink = login.items.toSignupLink;
-		const clickHandler = (event) => {
-			event.preventDefault();
-			this.goToPage(router.signup, true);
-		};
-		toSignupLink.addHandler('click', clickHandler);
 	}
 }
--- src/app/api.js
+++ src/app/api.js
@@ -1,100 +1,98 @@
-async function logout() {
-    try {
-        const response = await fetch('http://5.188.140.7:8080/logout', {
-            method: 'GET',
-            credentials: 'include' 
-        });
-        if (!response.ok) {
-            return false;
+function loginUser(login, password) {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/signin',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ 'username': login, 'password': password }),
+        xhrFields: {
+            withCredentials: true // для передачи cookies
         }
+    })
+    .done(function(response) {
         return true;
-    }
-    catch (error){
-        //console.log('error logout')
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.error('Ошибка:', textStatus, errorThrown);
         return false;
-    }
+    });
 }
 
-async function loginUser(login, password) {
-    try {
-        //console.log(login, password);
-        const response = await fetch('http://5.188.140.7:8080/signin', { 
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials : 'include',
-            body: JSON.stringify({'username': login, 'password': password}),
-        });
-
-        if (!response.ok) {
-            return false
+function checkAuth() {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/checkauth',
+        type: 'GET',
+        xhrFields: {
+            withCredentials: true // для передачи cookies
         }
-
-        //console.log('Успешно авторизовался');
-        await renderNavBar();
-        navigateTo('/feed');
-        return true
-    } catch (error) {
-        // console.error('Ошибка:', error);
-        return false
-    }
+    })
+    .done(function(response) {
+        // Если запрос успешен, возвращаем true
+        return true;
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // Обработка ошибки
+        console.error('Ошибка авторизации:', textStatus, errorThrown);
+        return false;
+    });
 }
 
-async function checkAuth() {
-    try {
-        const response = await fetch('http://5.188.140.7:8080/checkauth', {
-            method: 'GET',
-            credentials: 'include' 
-            });
-        if (!response.ok) {
-            return false;
+function logout() {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/logout',
+        method: 'GET',
+        xhrFields: {
+            withCredentials: true 
         }
-        return true;
-    }
-    catch (error){
-        //console.log('error auth')
-        return false;
-    }
+    })
+    .done(function() {
+        console.log('Успешный выход из системы');
+        return true; 
+    })
+    .fail(function(jqXHR) {
+        console.error('Ошибка при выходе:', jqXHR.statusText);
+        return false; 
+    });
 }
 
 async function fetchUsers() {
     try {
-      const response = await fetch('http://5.188.140.7:8080/getusers', {
+        const response = await fetch('http://5.188.140.7:8080/getusers', {
         method: 'GET',
         credentials: 'include',
         mode: 'cors'
-      });
-      if (!response.ok) {
-        throw new Error('Ошибка при получении списка пользователей');
-      }
-      return await response.json();
+        });
+        if (!response.ok) {
+            throw new Error('Ошибка при получении списка пользователей');
+        }
+        return await response.json();
     } catch (error) {
-      console.error('Ошибка при получении списка пользователей:', error);
-      return [];
+        console.error('Ошибка при получении списка пользователей:', error);
+        return [];
     }
 }
 
-async function registerUser(login, password, gender, age) {
-    try {
-      //console.log(login, password, gender, age);
-      const response = await fetch('http://5.188.140.7:8080/signup', { 
+function registerUser(login, password, gender, age) {
+    return $.ajax({
+        url: 'http://5.188.140.7:8080/signup',
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
+        contentType: 'application/json',
+        xhrFields: {
+            withCredentials: true // Для отправки куки с запросом
         },
-        credentials : 'include',
-        body: JSON.stringify({'username': login, 'password': password, 'gender': gender, 'age': parseInt(age)}),
-      });
-    
-      if (!response.ok) {
-        // throw new Error('Ошибка регистрации');
-      }
-      //console.log('Успешно зарегистрировался');
-      await renderNavBar();
-      navigateTo('/feed');
-    
-    } catch (error) {
-      console.error('Ошибка:', error);
-    }
+        data: JSON.stringify({
+            username: login,
+            password: password,
+            gender: gender,
+            age: parseInt(age)
+        })
+    })
+    .done(function() {
+        console.log('Успешно зарегистрировался');
+        renderNavBar().then(() => {
+        navigateTo('/feed');
+        });
+    })
+    .fail(function(jqXHR) {
+        console.error('Ошибка регистрации:', jqXHR.statusText);
+    });
 }
