edf73bb21f7e5aaa2a1f0de1fa498f419ec4c182
Andrey <111575820+andrey-918@users.noreply.github.com>
2024-10-25 10:38:52 +0300

    Add files via upload

--- compile.js
+++ compile.js
@@ -1,20 +1,26 @@
 import { compileFile } from 'pug';
 import { readdir, writeFile } from 'node:fs/promises';
 
-const PATHS = ['./src/components/SwipeCard/', './src/pages/login/', './src/pages/signup/', './src/pages/feed/'];
+const PATHS = [
+	'./src/components/SwipeCard/', 
+	'./src/pages/login/', 
+	'./src/pages/signup/', 
+	'./src/pages/feed/'
+];
   
 const PATH_OUT = './src/templates/';
 
 for (const path of PATHS) {
-  readdir(path)
-    .then(async (res) => {
-      for (const fileName of res) {
-        if (fileName.split('.')[1] === 'pug') {
-          const name = fileName.replace('.pug', '');
+	readdir(path)
+		.then(async (res) => {
+			for (const fileName of res) {
+				if (fileName.split('.')[1] === 'pug') {
+					const name = fileName.replace('.pug', '');
 
-          const templateFunction = compileFile(path + fileName);
-          writeFile(PATH_OUT + name + '.js', 'export default ' + templateFunction);
-        }
-      }
-    });
+					const templateFunction = compileFile(path + fileName);
+					writeFile(PATH_OUT + name + '.js', 
+						'export default ' + templateFunction);
+				}
+			}
+		});
 }
