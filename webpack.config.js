9b1bcbd2e7bf3e59339700a8f4a0ea308f3efb06
andreykarganov <karganov.an@yandex.ru>
2024-10-27 13:36:39 +0300

    working on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-    },
+	mode: 'development',
+	entry: {
+		bundle: path.resolve(__dirname, 'src/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name][contenthash].js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'SparkIt',
+			filename: 'index.html',
+			template: 'src/index.html',
+		}),
+	]
 };
