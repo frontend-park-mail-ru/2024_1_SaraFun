7b22ffcceb84b93dae72a6d3d6bdbd5ed6558fd7
Elizabeth <liza777sem@icloud.com>
2024-10-28 17:08:17 +0300

    corrections in server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const session = require('express-session');
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import session from 'express-session';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
