96b98768ff40dde0dd75eef8607909afe3d19a22
andreykarganov <karganov.an@yandex.ru>
2024-10-30 10:04:14 +0300

    created web ts conf, changed paths in css

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import App from './modules/App.js';
-
-const root = document.querySelector('#root');
-
-const app = new App(root);
-await app.init();
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js',
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'src', 'index.html')}),
+            new webpack.ProgressPlugin(),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+ 
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+    }
+}
