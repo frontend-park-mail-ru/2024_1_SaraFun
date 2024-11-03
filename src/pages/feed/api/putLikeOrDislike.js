09d3f27c5d3de1b601730a97cac779a32eef6d36
Elizabeth <liza777sem@icloud.com>
2024-11-03 18:35:40 +0300

    fix bugs

--- src/pages/feed/api/putLikeOrDislike.js
+++ src/pages/feed/api/putLikeOrDislike.js
@@ -3,7 +3,7 @@ import { post } from '../../../shared/api/api.js';
 export async function putLikeOrDislike(like, userID) {
 	try {
 		if (like) { console.log('LIKE'); } else { console.log("DISLIKE"); }
-		const body = { 'like': like, 'userID': userID };
+		const body = { 'target_user_id': parseInt(userID), 'is_like': like };
 		const response = await post('http://5.188.140.7:8080/likes', body);
 		return true;
 
