8743ea60714e28e034476055e10f96c0ae35e27e
andreykarganov <karganov.an@yandex.ru>
2024-11-03 11:34:23 +0300

    created settings

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -2,27 +2,74 @@ import template from './ui/profile.pug';
 import Navbar from '../../widgets/Navbar/navbar.js';
 import './ui/profile.scss';
 
-
-/**
- * Class representing the profile page.
- */
 export class ProfilePage {
-	/**
-   * Creates an instance of ProfilePage.
-   * @param {Object} parent - The parent object.
-   */
-	constructor(parent) {
-		this.parent = parent;
-		this.parent.root.innerHTML = '';
-		this.parent.root.innerHTML = this.render();
-		this.navbar = new Navbar(document.querySelector('nav'), parent);
-	}
+  constructor(parent) {
+    this.parent = parent;
+    this.isEditing = false; 
+    this.render();
+  }
 
-	/**
-   * Renders the profile page template.
-   * @returns {string} - The HTML string of the profile page template.
-   */
-	render() {
-		return template();
-	}
+  // Функция для ограничения количества строк
+  limitText(textarea, limit) {
+    const limitLines = () => {
+      const lines = textarea.value.split("\n");
+      if (lines.length > limit) {
+        textarea.value = lines.slice(0, limit).join("\n");
+      }
+    };
+
+    limitLines();
+
+    let timeout;
+    textarea.addEventListener("input", function () {
+      clearTimeout(timeout);
+      timeout = setTimeout(limitLines, 1);
+    });
+  }
+
+  render() {
+    this.parent.root.innerHTML = template({ isEditing: this.isEditing });
+    this.navbar = new Navbar(document.querySelector('nav'), this.parent);
+    
+    const settingsButton = document.querySelector('.settings-button');
+    settingsButton.addEventListener('click', () => this.toggleEditMode());
+
+    const saveButton = document.getElementById('save-settings');
+    if (saveButton) {
+      saveButton.addEventListener('click', () => this.saveSettings());
+    }
+
+    const cancelButton = document.getElementById('cancel-settings');
+    if (cancelButton) {
+      cancelButton.addEventListener('click', () => this.toggleEditMode());
+    }
+
+    if (this.isEditing) {
+      const textarea = document.getElementById('bio'); 
+      this.limitText(textarea, 10); // Ограничение на кол-во строк
+
+      textarea.addEventListener('input', () => {
+        textarea.style.height = 'auto'; 
+        textarea.style.height = textarea.scrollHeight + 'px'; 
+      });
+    }
+  }
+
+  toggleEditMode() {
+    this.isEditing = !this.isEditing; 
+    this.render(); 
+  }
+  
+  saveSettings() {
+    const username = document.getElementById('username').value;
+    const gender = document.getElementById('gender').value;
+    const age = document.getElementById('age').value;
+    const bio = document.getElementById('bio').value;
+
+
+    // Здесь добавить логику для сохранения данных (отправка на сервер)
+
+    this.isEditing = false; 
+    this.render(); 
+  }
 }
