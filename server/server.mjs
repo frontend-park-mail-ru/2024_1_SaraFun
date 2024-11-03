d91cd479b3a049c9075ee7350255444fe1a83867
andreykarganov <karganov.an@yandex.ru>
2024-11-03 21:19:39 +0300

    cors

--- server/server.mjs
+++ server/server.mjs
@@ -1,21 +1,37 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+import session from 'express-session';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(cors({
+	origin: 'http://localhost:80', 
+	credentials: true
+}));
 
-app.get(/^(?!.*\.(css|js|img|png|webp|webm|svg)).*$/, (req, res) => {
-	res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+app.use(session({
+	secret: process.env.SECRET_KEY,
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: false } 
+}));
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
 });
 
-const port = 80;
-
-app.listen(port, () => {
-	console.info(`Сервер запущен на порту ${port}`);
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
