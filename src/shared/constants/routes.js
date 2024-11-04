b18d8143be51c0739fecfd6b39f5b267c52cf15d
Elizabeth <liza777sem@icloud.com>
2024-11-04 17:20:23 +0300

    add matches

--- src/shared/constants/routes.js
+++ src/shared/constants/routes.js
@@ -1,15 +1,18 @@
 import { FeedPage } from "../../pages/feed/feed.js"
 import { LoginPage } from "../../pages/login/login.js";
 import { RegistrationPage } from "../../pages/signup/signup.js";
+import { MatchesPage } from "../../pages/matches/matches.js";
 
 export const ROUTES_NAME = {
     LOGIN: 'login',
     FEED: 'feed',
+    MATCHES: 'matches',
     SIGNUP: 'signup',
 };
 
 export const ROUTES = new Map([
     [ROUTES_NAME.LOGIN, { path: '/login', view: LoginPage }],
     [ROUTES_NAME.FEED, { path: '/feed', view: FeedPage }],
+    [ROUTES_NAME.MATCHES, { path: '/matches', view: MatchesPage }],
     [ROUTES_NAME.SIGNUP, { path: '/signup', view: RegistrationPage }],
 ]);
