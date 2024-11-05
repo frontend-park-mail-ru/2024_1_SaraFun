922965b331011887c56743d04ad526675614895a
Elizabeth <liza777sem@icloud.com>
2024-11-05 17:53:54 +0300

    fix like request

--- src/pages/feed/api/putLikeOrDislike.js
+++ src/pages/feed/api/putLikeOrDislike.js
@@ -3,7 +3,7 @@ import { post } from '../../../shared/api/api.js';
 export async function putLikeOrDislike(like, userID) {
 	try {
 		const body = { "receiver": parseInt(userID), "type": like };
-		const response = await post('http://5.188.140.7:8080/reaction', body);
+		const response = await post('http://5.188.140.7:8080/reaction', JSON.stringify(body));
 		return true;
 
 	} catch (error) {
