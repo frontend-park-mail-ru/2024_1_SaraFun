27bc664013aaf963beee7e22f511dd8900e2c8e7
Elizabeth <liza777sem@icloud.com>
2024-11-07 20:27:46 +0300

    make carousel component

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -21,29 +21,6 @@ export class FeedPage {
 		});
 	}
 
-	/*showImage(container, index) {
-		const images = container.querySelectorAll('.image-section__img');
-		images.forEach((img, i) => {
-			img.style.display = i === index ? 'block' : 'none';
-		});
-	}
-
-	scrollLeft(container) {
-		const images = container.querySelectorAll('.image-section__img');
-		let currentIndex = parseInt(container.getAttribute('data-current-index')) || 0;
-		currentIndex = (currentIndex - 1 + images.length) % images.length;
-		container.setAttribute('data-current-index', currentIndex);
-		this.showImage(container, currentIndex);
-	}
-
-	scrollRight(container) {
-		const images = container.querySelectorAll('.image-section__img');
-		let currentIndex = parseInt(container.getAttribute('data-current-index')) || 0;
-		currentIndex = (currentIndex + 1) % images.length; 
-		container.setAttribute('data-current-index', currentIndex);
-		this.showImage(container, currentIndex);
-	}*/
-
 	/**
      * Renders the feed page by fetching users and initializing cards.
      * @returns {Promise<void>} - A promise that resolves when the rendering is complete.
