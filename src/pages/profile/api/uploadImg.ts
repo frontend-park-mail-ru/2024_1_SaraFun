71b829b86aa0e2d0f1ac12935c42eb0e29a13f84
Elizabeth <liza777sem@icloud.com>
2024-11-18 18:49:29 +0300

    fix request uploadImage

--- src/pages/profile/api/uploadImg.ts
+++ src/pages/profile/api/uploadImg.ts
@@ -1,11 +1,12 @@
 import { postFormData } from '../../../shared/api/api';
 import { ImgData } from './profile';
+
 export async function uploadImg(imagesNew: ImgData[], imagesURLs: string[], imagesIndexes: number[]): Promise<boolean> {
     try {
         for (const image of imagesNew) {
             const formData = new FormData();
             formData.append('image', image.file);
-            const response = await postFormData('http://5.188.140.7:8080/uploadimage', formData);
+            const response = await postFormData('/uploadimage', formData);
             if (!response.ok) {
               console.error('Failed to upload the image:', response.statusText);
               return false; 
