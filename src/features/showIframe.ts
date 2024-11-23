5806990f838dad39902cde037ade57e20a1319cf
Elizabeth <liza777sem@icloud.com>
2024-11-23 13:54:08 +0300

    try add cookies

--- src/features/showIframe.ts
+++ src/features/showIframe.ts
@@ -1,5 +1,15 @@
 export function toggleCsatIframe(show: boolean) {
-    const csatIframe = document.getElementById('csat-iframe');
+    const csatIframe = document.getElementById('csat-iframe') as HTMLIFrameElement;
+    const userCookies = document.cookie;
+
+    csatIframe.contentWindow.postMessage(
+        {
+            msg: 'set-cookies',
+            cookies: userCookies
+        },
+        '*'
+    );
+    
     //console.log(csatIframe);
     if (show) {
         csatIframe.style.display = 'block';
@@ -12,5 +22,14 @@ export function toggleCsatIframe(show: boolean) {
 window.addEventListener('message', function(event) {
     if (event.data === 'close-iframe') {
         toggleCsatIframe(false);
+    }
+
+    if (event.data.msg === 'set-cookies') {
+        const cookies = event.data.cookies;
+        const cookiesArray = cookies.split('; ');
+        cookiesArray.forEach((cookie: string) => {
+          document.cookie = cookie;
+        });
+        console.log('Cookies set in iframe:', document.cookie);
     }
 });
