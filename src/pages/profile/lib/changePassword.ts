01c9e908258b7eb5c71cd27551aedcdf1463a8d3
andreykarganov <karganov.an@yandex.ru>
2024-11-24 21:55:33 +0300

    pass validation

--- src/pages/profile/lib/changePassword.ts
+++ src/pages/profile/lib/changePassword.ts
@@ -1,38 +1,44 @@
+import { isValidPassword } from "../../../shared/utils/validation";
 import { saveNewPassword } from "../api/changePassword";
 import { validateOldPassword } from "../api/validOldPassword";
 
 export class PasswordChanger {
-  constructor(button: HTMLElement) {
+    constructor(button: HTMLElement) {
     if (button) {
-      button.addEventListener('click', () => this.changePassword());
+        button.addEventListener('click', () => this.changePassword());
     }
-  }
-
-  async changePassword() {
-    const oldPassword = (document.getElementById('OldPassword') as HTMLInputElement).value;
-    const newPassword = (document.getElementById('NewPassword') as HTMLInputElement).value;
-    const repeatNewPassword = (document.getElementById('RepeatNewPassword') as HTMLInputElement).value;
-
-    if (!this.validatePasswords(newPassword, repeatNewPassword)) {
-      alert('Новый пароль и его повторение не совпадают.');
-      return;
     }
 
-    try {
-      const isOldPasswordValid = await validateOldPassword(oldPassword);
-      if (!isOldPasswordValid) {
-        alert('Старый пароль неверный.');
-        return;
-      }
-      await saveNewPassword(newPassword);
-      alert('Пароль успешно изменен.');
-    } catch (error) {
-      console.error('Ошибка при смене пароля:', error);
-      alert('Произошла ошибка при смене пароля. Попробуйте еще раз.');
-    }
-  }
+    async changePassword() {
+        const oldPassword = (document.getElementById('OldPassword') as HTMLInputElement).value;
+        const newPassword = (document.getElementById('NewPassword') as HTMLInputElement).value;
+        const repeatNewPassword = (document.getElementById('RepeatNewPassword') as HTMLInputElement).value;
 
-  validatePasswords(newPassword: string, repeatNewPassword: string): boolean {
-    return newPassword === repeatNewPassword;
-  }
+
+        if (!isValidPassword(newPassword)) {
+            alert('Новый пароль не удовлетворяет условиям.');
+            return;
+        }
+        if (!this.validatePasswords(newPassword, repeatNewPassword)) {
+            alert('Новый пароль и его повторение не совпадают.');
+            return;
+        }
+
+        try {
+            const isOldPasswordValid = await validateOldPassword(oldPassword);
+            if (!isOldPasswordValid) {
+            alert('Старый пароль неверный.');
+            return;
+            }
+            await saveNewPassword(newPassword);
+            alert('Пароль успешно изменен.');
+        } catch (error) {
+            console.error('Ошибка при смене пароля:', error);
+            alert('Произошла ошибка при смене пароля. Попробуйте еще раз.');
+        }
+    }
+
+    validatePasswords(newPassword: string, repeatNewPassword: string): boolean {
+        return newPassword === repeatNewPassword;
+    }
 }
