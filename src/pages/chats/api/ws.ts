a670a26ec5e7b66e5f27bd63b230480f643615d4
Elizabeth <liza777sem@icloud.com>
2024-11-25 00:19:16 +0300

    try fix ws

--- src/pages/chats/api/ws.ts
+++ src/pages/chats/api/ws.ts
@@ -1,8 +1,21 @@
 import {get} from '../../../shared/api/api';
+import { BASE_URL } from "../../../shared/constants/baseURL";
 
 export async function ws(): Promise<any> {
 	try {
-		const response = await get('/ws');
+        const options: RequestInit = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Connection': 'Upgrade',
+                'Upgrade': 'websocket'
+            },
+            credentials: 'include'
+        };
+        const response = await fetch(`${BASE_URL}/ws`, options);
+        if (!response.ok) {
+            throw new Error(`${response.status}: ${response.statusText}`);
+        }
 		const previews: any = await response.json();
 		return previews;
 
