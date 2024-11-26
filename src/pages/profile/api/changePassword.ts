81ac4e1bccbf7472cc674c51572bed7540254657
andreykarganov <karganov.an@yandex.ru>
2024-11-26 20:09:09 +0300

    fix

--- src/pages/profile/api/changePassword.ts
+++ src/pages/profile/api/changePassword.ts
@@ -7,15 +7,16 @@ export async function saveNewPassword(oldPassword: string, newPassword: string) 
     const response = await post('/changepassword', body);
 
     if (!response.ok) {
+      if (response.status === 412) {
+        notificationManager.addNotification('Старый пароль неверный. Пожалуйста, попробуйте снова.', 'fail');
+        return false;
+      }
       return false
     }
     notificationManager.addNotification('Пароль успешно изменен.', 'success');
 
     return true; 
   } catch (error) {
-    if (error === 'Error: 412: Precondition Failed') {
-      return false;
-    }
     console.error('Ошибка при смене пароля:', error);
     notificationManager.addNotification('Произошла ошибка при смене пароля. Попробуйте еще раз.', 'fail');
     return true;
