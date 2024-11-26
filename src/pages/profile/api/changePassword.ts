a0dd490165df7bbc7186a95d2ce70b42972dcebe
andreykarganov <karganov.an@yandex.ru>
2024-11-26 20:12:05 +0300

    fix

--- src/pages/profile/api/changePassword.ts
+++ src/pages/profile/api/changePassword.ts
@@ -7,18 +7,14 @@ export async function saveNewPassword(oldPassword: string, newPassword: string) 
     const response = await post('/changepassword', body);
 
     if (!response.ok) {
-      if (response.status === 412) {
-        notificationManager.addNotification('Старый пароль неверный. Пожалуйста, попробуйте снова.', 'fail');
-        return false;
-      }
       return false
     }
-    notificationManager.addNotification('Пароль успешно изменен.', 'success');
 
+    notificationManager.addNotification('Пароль успешно изменен.', 'success');
     return true; 
   } catch (error) {
     console.error('Ошибка при смене пароля:', error);
     notificationManager.addNotification('Произошла ошибка при смене пароля. Попробуйте еще раз.', 'fail');
-    return true;
+    return false;
   }
 }
