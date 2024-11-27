e5c424c721b8b35580d16fbb2a2f61c5a9b53516
Elizabeth <liza777sem@icloud.com>
2024-11-27 14:29:13 +0300

    fix entity ChatPeview

--- src/entities/ChatPreview/ChatPreview.ts
+++ src/entities/ChatPreview/ChatPreview.ts
@@ -5,8 +5,10 @@ export interface ChatPreview {
     last_name: string;
     images: Image[];
     last_message: string;
+    message: string,
     self: boolean;
     time: string;
+    by_message: boolean;
 }
 
 export interface Image {
