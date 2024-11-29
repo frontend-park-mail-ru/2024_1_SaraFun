1e762ebc77b69d54a955a29bdc2a96879849f008
Elizabeth <liza777sem@icloud.com>
2024-11-29 03:44:59 +0300

    add logs

--- src/app/getParams.ts
+++ src/app/getParams.ts
@@ -1,7 +1,9 @@
 export function getParams(): { [key: string]: string } {
     const params: { [key: string]: string } = {};
     const queryString = window.location.search;
+    console.log(queryString);
     const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams);
 
     urlParams.forEach((value, key) => {
         params[key] = value;
