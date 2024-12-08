800124f5ab58b8532b4d3004dae7f4cf1052c4bd
andreykarganov <karganov.an@yandex.ru>
2024-12-08 19:39:38 +0300

    del console and title

--- sw.js
+++ sw.js
@@ -11,10 +11,10 @@ self.addEventListener('install', (event) => {
   console.log('Service Worker: Installing...');
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Service Worker: Caching files');
-      URLS_TO_CACHE.forEach(url => {
-        console.log(`Caching ${url}`);
-      });
+      // console.log('Service Worker: Caching files');
+      // URLS_TO_CACHE.forEach(url => {
+      //   console.log(`Caching ${url}`);
+      // });
       return cache.addAll(URLS_TO_CACHE).catch((error) => {
         console.error('Failed to cache:', error);
       });
@@ -29,7 +29,7 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Service Worker: Removing old cache', cacheName);
+            // console.log('Service Worker: Removing old cache', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -39,15 +39,15 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Service Worker: Fetching...', event.request.url);
+  // console.log('Service Worker: Fetching...', event.request.url);
   
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
-        console.log('Service Worker: Serving from cache', event.request.url);
+        // console.log('Service Worker: Serving from cache', event.request.url);
         return response;
       }
-      console.log('Service Worker: Fetching from network', event.request.url);
+      // console.log('Service Worker: Fetching from network', event.request.url);
       return fetch(event.request).catch(() => {
         return caches.match('/offline.html');
       });
