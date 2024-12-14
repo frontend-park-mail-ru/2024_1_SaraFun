c8e2dd0d9c4482d306ee78882b87589be8824a9b
andreykarganov <karganov.an@yandex.ru>
2024-12-14 20:48:31 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,88 +3,90 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	entry: {
-		bundle: path.resolve(__dirname, 'src/index.ts'),
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/',
-		filename: '[name][contenthash].js',
-		clean: true,
-		assetModuleFilename: '[name][ext]',
-	},
-	devtool: 'source-map',
-	devServer: {
-		static: {
-			directory: path.resolve(__dirname, 'dist')
-		},
-		port: 4000,
-		open: {
-			app: {
-				name: 'Google Chrome',
-			},
-		},
-		hot: true,
-		compress: true,
-		historyApiFallback: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /.(scss|css)$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			},
-			{
-				test: /.(png|jpg|jpeg|gif|svg)$/i,
-				type: 'asset/resource',
-				generator: {
-					filename: 'img/[name][ext]',
-				},
-			},
-			{
-				test: /.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/resource',
-				generator: {
-					filename: 'fonts/[name][ext]',
-				},
-			},
-			{
-				test: /.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /.pug$/,
-				loader: 'pug-loader',
-			},
-		],
-	},
-	resolve: {
-		fallback: {
-			"http": require.resolve("stream-http"),
-			"https": require.resolve("https-browserify"),
-			"assert": require.resolve("assert/"),
-		},
-		alias: {
-			'@img': path.resolve(__dirname, 'src/shared/assets/img')
-		},
-		extensions: ['.tsx', '.ts', '.js', '.pug', '.svg'],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'SparkIt',
-			filename: 'index.html',
-			template: 'src/index.html',
-		}),
-		new CopyWebpackPlugin({
-			patterns: [
-				{ from: 'sw.js', to: 'sw.js' },
-			],
-		}),
-	]
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 4000,
+        open: {
+            app: {
+                name: 'Google Chrome',
+            },
+        },
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /.(scss|css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'img/[name][ext]',
+                },
+            },
+            {
+                test: /.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
+            },
+            {
+                test: /.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /.pug$/,
+                loader: 'pug-loader',
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+            "os": require.resolve("os-browserify/browser"),
+            "crypto": require.resolve("crypto-browserify"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "assert": require.resolve("assert/"),
+        },
+        alias: {
+            '@img': path.resolve(__dirname, 'src/shared/assets/img')
+        },
+        extensions: ['.tsx', '.ts', '.js', '.pug', '.svg'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'SparkIt',
+            filename: 'index.html',
+            template: 'src/index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'sw.js', to: 'sw.js' },
+            ],
+        }),
+    ]
 };
