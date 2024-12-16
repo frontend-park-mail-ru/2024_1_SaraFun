b07fea5433e59d55c95f5f11a5ec7a177647531b
Elizabeth <liza777sem@icloud.com>
2024-12-16 03:41:11 +0300

    try fix request in feed

--- src/pages/feed/api/putLikeOrDislike.ts
+++ src/pages/feed/api/putLikeOrDislike.ts
@@ -1,21 +1,18 @@
 import { post } from '../../../shared/api/api';
 
-export async function putLikeOrDislike(like: boolean, userID: number): Promise<boolean> {
+export async function putLikeOrDislike(like: boolean, userID: number): Promise<string | boolean> {
 	try {
 		const body = { 'receiver': userID, 'type': like };
 		await post('/api/communications/reaction', body);
 		return true;
 
 	} catch (error) {
-		if (error instanceof Response) {
-            try {
-                const errorData = await error.json();
-                console.error('Сообщение об ошибке:', errorData);
-            } catch (jsonError) {
-                console.error('Ошибка при парсинге JSON:', jsonError);
-            }
-        }
 		console.error(error);
+		if (error instanceof Error) {
+            const errorMessage = error.message.split(', ').pop();
+            console.error('Сообщение об ошибке:', errorMessage);
+            return errorMessage;
+        }
 		return false;
 	}
 }
