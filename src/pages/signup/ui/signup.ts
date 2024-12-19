9e948599b251387a129033aea4306f49c5905177
Elizabeth <liza777sem@icloud.com>
2024-12-19 13:05:13 +0300

    add tags

--- src/pages/signup/ui/signup.ts
+++ src/pages/signup/ui/signup.ts
@@ -10,6 +10,11 @@ import { limitInput } from '../../../features/limitInput';
  */
 export class RegistrationPage {
 	private parent: Router;
+	private login: string;
+	private password: string;
+	private first_name: string;
+	private gender: string;
+	private birth_date: string;
 	/**
    * Creates an instance of RegistrationPage.
    * @param {Object} parent - The parent object.
@@ -59,66 +64,95 @@ export class RegistrationPage {
 
 		this.limits();
 
-		this.addInputListeners();
-
-	
-		document.querySelector('.signup-button').addEventListener('click', async () => {
-			const login = (document.getElementById('login') as HTMLInputElement).value;
-			const password = (document.getElementById('password') as HTMLInputElement).value;
-			const first_name = (document.getElementById('first_name') as HTMLInputElement).value; 
-			const gender = (document.querySelector('input[name="gender"]:checked') as HTMLInputElement).value;
-			const birth_date_element = document.getElementById('birth_date') as HTMLInputElement; 
-			const birth_date = (document.getElementById('birth_date') as HTMLInputElement).value;
-			let valid = true;
+		const nextButton = document.querySelector('.next-button') as HTMLElement;
+		const step1 = document.querySelector('.step1') as HTMLElement;
+		const step2 = document.querySelector('.step2') as HTMLElement;
+		
+		if (nextButton) {
+			nextButton.addEventListener('click', () => {
+				const login = (document.getElementById('login') as HTMLInputElement).value;
+				const password = (document.getElementById('password') as HTMLInputElement).value;
+				const gender = (document.querySelector('input[name="gender"]:checked') as HTMLInputElement).value;
+				const birth_date_element = document.getElementById('birth_date') as HTMLInputElement; 
+				const birth_date = (document.getElementById('birth_date') as HTMLInputElement).value;
+				let valid = true;
 			
-			if (!/\d{4}-\d{2}-\d{2}/.test(birth_date)) {
-				valid = false;
-				notificationManager.addNotification('Некорректный формат даты. Используйте YYYY-MM-DD.', 'fail');
-				return;
-			}
+				if (!/\d{4}-\d{2}-\d{2}/.test(birth_date)) {
+					valid = false;
+					notificationManager.addNotification('Некорректный формат даты. Используйте YYYY-MM-DD.', 'fail');
+					return;
+				}
 
-			if (birth_date_element) {
-				const isValidDate = isValidBirthDate(birth_date_element);
-				if (!isValidDate) {
-					document.getElementById(`date-error`).style.display = 'block';
-					document.getElementById(`date-error`).innerText = 'Вам должно быть от 18 до 120 лет';
+				if (birth_date_element) {
+					const isValidDate = isValidBirthDate(birth_date_element);
+					if (!isValidDate) {
+						document.getElementById(`date-error`).style.display = 'block';
+						document.getElementById(`date-error`).innerText = 'Вам должно быть от 18 до 120 лет';
+						valid = false;
+					}
+				}
+
+				const loginErrors = isValidLogin(login);
+				const passwordErrors = isValidPassword(password);
+
+				if (passwordErrors.length > 0) {
+					passwordErrors.forEach((error, index) => {
+						document.getElementById(`password-error-${index + 1}`).innerText = error;
+						document.getElementById(`password-error-${index + 1}`).style.display = 'block';
+					});
+				}	
+			  
+				if (loginErrors.length > 0) {
+					loginErrors.forEach((error, index) => {
+						document.getElementById(`login-error-${index + 1}`).innerText = error;
+						document.getElementById(`login-error-${index + 1}`).style.display = 'block';
+					});
+				}
+			 
+			
+				if (loginErrors.length > 0 || passwordErrors.length > 0 || !valid) {
+					notificationManager.addNotification('Пожалуйста, исправьте ошибки в форме.', 'fail');
 					valid = false;
 				}
-			}
+
+				if (valid) {
+					this.login = login;
+					this.password = password;
+					this.gender = gender;
+					this.birth_date = birth_date;
+					step1.style.display = 'none';
+					step2.style.display = 'flex';
+				}
+			});
+		}
+		
+		const interestTags = document.querySelectorAll('.interest-tag') as NodeListOf<HTMLElement>;
+		interestTags.forEach(tag => {
+			tag.addEventListener('click', () => {
+			tag.classList.toggle('selected');
+			});
+		});
+
+		this.addInputListeners();
+
+		const signupButton = document.querySelector('.signup-button') as HTMLElement;
+		signupButton.addEventListener('click', async () => {
+			const first_name = (document.getElementById('first_name') as HTMLInputElement).value; 
+			let valid = true;
 
 			if (first_name === '') {
 				this.showError('first-name-error', 'Имя не может быть пустым полем');
 				valid = false;
 			}
-
-			const loginErrors = isValidLogin(login);
-			const passwordErrors = isValidPassword(password);
-
-			if (passwordErrors.length > 0) {
-				passwordErrors.forEach((error, index) => {
-					document.getElementById(`password-error-${index + 1}`).innerText = error;
-					document.getElementById(`password-error-${index + 1}`).style.display = 'block';
-				});
-			}	
-			  
-			if (loginErrors.length > 0) {
-				loginErrors.forEach((error, index) => {
-					document.getElementById(`login-error-${index + 1}`).innerText = error;
-					document.getElementById(`login-error-${index + 1}`).style.display = 'block';
-				});
-			}
-			 
 			
-			if (loginErrors.length > 0 || passwordErrors.length > 0 || first_name === '' || !valid) {
+			if (first_name === '') {
 				notificationManager.addNotification('Пожалуйста, исправьте ошибки в форме.', 'fail');
 				valid = false;
 			}
 
-						
-		
 			if (valid) {
 				try {
-					const isSignedUp = await signupUser(login, password, first_name, gender, birth_date);
+					const isSignedUp = await signupUser(this.login, this.password, first_name, this.gender, this.birth_date);
 					if (!isSignedUp) {
 						notificationManager.addNotification('Логин уже занят, попробуйте другой', 'fail'); 
 					} else { 
